a0baefd7dc4d98d25fbd856719c9db72fd889e1f
Nikunj Patel <nikunj.patel@hotmail.com>
2020-05-21 20:58:43 +0530

    Added new test case

--- test.js
+++ test.js
@@ -5,5 +5,9 @@ describe("Random number generator", () => {
 test('It should run with valid input', () => {
     expect(randomNumber(1, 10)).toBeTruthy();
 });
+
+test('It should run with valid input', () => {
+    expect(randomNumber("nikunj", "patel")).toBeTruthy();
+});
   
 });
