73a239387389adfad24ea4641c5a88062c777b98
Nikunj Patel <nikunj.patel@hotmail.com>
2020-05-21 20:53:18 +0530

    Included test case

---
+++ test.js
@@ -0,0 +1,9 @@
+const randomNumber = require("./index.js");
+
+describe("Random number generator", () => {
+
+test('It should run with valid input', () => {
+    expect(randomNumber(1, 10)).toBeTruthy();
+});
+  
+});
