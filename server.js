99568258db9e6b50146957f5526d085e714d73a1
Niknuj Patel <nikunj.patel@hotmail.com>
2020-06-06 18:01:57 +0530

    Docker config

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const randomNoGenerator = require('./src/index');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World, Lets generate a random number ' + randomNoGenerator(1,10));
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
