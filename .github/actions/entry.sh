08f7f06689ea34d4c38ca53a9eed1541cd9893d0
Niknuj Patel <nikunj.patel@hotmail.com>
2020-06-07 17:25:36 +0530

    NP: change permission

--- .github/actions/entry.sh
+++ .github/actions/entry.sh
@@ -15,7 +15,6 @@ fi
 
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY
-export AWS_DEFAULT_REGION=us-west-2
 
 aws s3 cp --metadata-directive REPLACE --cache-control "public, max-age=86400" --quiet --recursive $BUNDLE_LOCATION s3://$BUCKET_NAME/production/$TAG/
 
