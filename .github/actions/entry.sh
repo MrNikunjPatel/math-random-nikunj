c0156ff87776714e59f922e3bda43de71f9f41ad
Niknuj Patel <nikunj.patel@hotmail.com>
2020-06-07 14:42:02 +0530

    Change key name

--- .github/actions/entry.sh
+++ .github/actions/entry.sh
@@ -8,8 +8,8 @@ if [ -z "$TAG" ] : then
     exit 1
 fi
 
-export AWS_ACCESS_KEY=$AWS_ACCESS_KEY
-export AWS_SECRET_KEY=$AWS_SECRET_KEY
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY
 
 aws s3 cp --metadata-directive REPLACE --cache-control "public, max-age=86400" --quiet --recursive $BUNDLE_LOCATION s3://$BUCKET_NAME/production/$TAG/
 
