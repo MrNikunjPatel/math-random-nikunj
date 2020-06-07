a3d7e240bbdfe1c4692399059cc67e203a30ea37
Niknuj Patel <nikunj.patel@hotmail.com>
2020-06-07 15:19:40 +0530

    remove tag

--- .github/actions/entry.sh
+++ .github/actions/entry.sh
@@ -1,17 +1,18 @@
 #!/bin/dash
 echo "Replacing ${GITHUB_REF}"
 
-TAG=${GITHUB_REF/refs\/tags\//}
+# TAG=${GITHUB_REF/refs\/tags\//}
 
-if [ -z "$TAG" ] : then
-    echo "No tag found"
-    exit 1
-fi
+# if [ -z "$TAG" ] : then
+#     echo "No tag found"
+#     exit 1
+# fi
 
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY
 
-aws s3 cp --metadata-directive REPLACE --cache-control "public, max-age=86400" --quiet --recursive $BUNDLE_LOCATION s3://$BUCKET_NAME/production/$TAG/
+# aws s3 cp --metadata-directive REPLACE --cache-control "public, max-age=86400" --quiet --recursive $BUNDLE_LOCATION s3://$BUCKET_NAME/production/$TAG/
+aws s3 cp --metadata-directive REPLACE --cache-control "public, max-age=86400" --quiet --recursive $BUNDLE_LOCATION s3://$BUCKET_NAME/production/
 
 echo "Finished CDN Deploy"
 exit 0
